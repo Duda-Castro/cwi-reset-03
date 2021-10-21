6bf1553071407b163f766ad7f0d60bd7b7f80b4d
Eduardo Anhaia Castro <Eduardo.castro1@hotmail.com>
2021-10-21 17:47:32 -0300

    exceptions adicionais inseridas, request filme e personagem, filme controller filme service e personagem service

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/dudacastro/exception/TipoDominioException.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/dudacastro/exception/TipoDominioException.java
@@ -1,12 +1,11 @@
 package br.com.cwi.reset.dudacastro.exception;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public enum TipoDominioException {
 
     ATOR("ator", "atores"),
-    DIRETOR("diretor", "diretores");
+    DIRETOR("diretor", "diretores"),
+    ESTUDIO("estúdio", "estúdios"),
+    FILME("filme", "filmes");
 
     private final String singular;
     private final String plural;
