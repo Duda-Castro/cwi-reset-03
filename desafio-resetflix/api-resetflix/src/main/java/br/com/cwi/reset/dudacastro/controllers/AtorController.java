a01f01e4462ed24fdba4422c99c0ff4412bda400
Eduardo Anhaia Castro <Eduardo.castro1@hotmail.com>
2021-10-21 15:23:09 -0300

    Ator controller concluido

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/dudacastro/controllers/AtorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/dudacastro/controllers/AtorController.java
@@ -2,10 +2,14 @@ package br.com.cwi.reset.dudacastro.controllers;
 
 import br.com.cwi.reset.dudacastro.FakeDatabase;
 import br.com.cwi.reset.dudacastro.exception.CampoNaoInformadoException;
+import br.com.cwi.reset.dudacastro.model.Ator;
 import br.com.cwi.reset.dudacastro.request.AtorRequest;
+import br.com.cwi.reset.dudacastro.response.AtorEmAtividade;
 import br.com.cwi.reset.dudacastro.service.AtorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/atores")
@@ -28,6 +32,21 @@ public class AtorController {
             campo = "Nome";
             throw new CampoNaoInformadoException(campo);
         }
+    }
+
+    @GetMapping(path = "/em_atividade")
+    public List<AtorEmAtividade> listarAtoresEmAtividade(@RequestParam String filtroNome) throws Exception {
+        return this.atorService.listarAtoresEmAtividade(filtroNome);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Ator consultarAtor(@PathVariable Integer id) throws Exception {
+        return this.atorService.consultarAtor(id);
+    }
+
+    @GetMapping
+    public List<Ator> consultarAtores() throws Exception {
+        return this.atorService.consultarAtores();
     }
 }
 
